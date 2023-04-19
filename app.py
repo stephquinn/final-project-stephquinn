4ce10cdc30180c6d3735c95adb23bb83dfee6de3
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-04-19 00:40:27 +0000

    trying to get compliance data to display

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ app = Flask(__name__)
 db = SqliteDatabase('wsa.db')
 
 class Inspection(Model):
-    url= TextField()
-    ai_id= IntegerField()
+    ai_id= IntegerField(unique=True)
     site_name= CharField()
-    addressinfo= CharField()
     city_state_zip= CharField()
     county= CharField()
     inspection_type= CharField()
-    fir_inspection_date= DateField()
+    fir_inspection_date= DateTimeField()
     permit_no= CharField()
     npdes_no= CharField()
     site_status= CharField()
@@ -26,23 +24,34 @@ class Inspection(Model):
         table_name = "inspections"
         database = db
 
-class EnforcementAction(Model):
-    #START HERE
+class Enforcement(Model):
+    ai_id: IntegerField(unique=True)
+    ai_name: CharField()
+    city_state_zip: CharField()
+    county: CharField()
+    enforcement_action: CharField()
+    enforcement_action_number: CharField()
+    enforcement_action_issued: DateTimeField()
+    case_closed: DateTimeField()
+    media: CharField()
+    program: CharField()
 
-@app.route("/")
-def wsa_compliance():
+    class Meta:
+        table_name = "enforcements"
+        database = db
+
+def get_compliance():
     url = "https://opendata.maryland.gov/resource/hxmu-urvx.json"
     response = requests.get(url)
     wsa_compliance = response.json()
     return wsa_compliance
 
-@app.route("/sitename/<slug>")
-
-
 @app.route("/")
 def index():
-    template = 'index.html'
-    return render_template(template)
+    inspection_count = Inspection.select().count()
+    template= "index.html"
+    return render_template(template, count=inspection_count)
+
 
 if __name__ == '__main__':
     # Fire up the Flask test server
