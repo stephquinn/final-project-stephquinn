c37a1b6b499717c7f5a7cc023dc72dc66d4d6fc8
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-05-06 16:11:44 +0000

    pandas to update actions dates why not

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ rm wsa.db
 
 sqlite-utils insert wsa.db inspections static/compliance2.csv --csv
 
-sqlite-utils insert wsa.db actions static/actions.csv --csv
+sqlite-utils insert wsa.db actions static/actions2.csv --csv
 
 
 
--- update_data.py
+++ update_data.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import pandas as pd
 
 rows = []
 
@@ -14,16 +15,11 @@ with open("static/compliance2.csv", "w") as output:
     writer.writerow(["document","site_no","site_name","street_address","city_state_zip","county","inspection_type","inspection_date","permit_no","npdes_no","complaint_tracking_no","inspection_reason","site_status","site_condition","recommended_actions","compliance_assist"])
     writer.writerows(rows)
 
-donkeys = []
 
-with open("static/actions.csv", "r") as file:
-    reader = csv.DictReader(file)
-    for donkey in reader:
-        if donkey['enforcement_action_issued'] != 'NA':
-            donkey['enforcement_action_issued'] = datetime.datetime.strptime(donkey['enforcement_action_issued'], '%m/%d/%Y').strftime('%Y-%m-%d')
-            donkeys.append(row.values())
+df = pd.read_csv("static/actions.csv", na_values=["NA"])
 
-with open("static/actions2.csv", "w") as file:
-    writer = csv.writer(output)
-    writer.writerow(["document","site_no","site_name","street_address","city_state_zip","county","enforcement_action","enforcement_action_number","enforcement_action_issued","case_closed","media","program"])
-    writer.writerows(rows)
+df["enforcement_action_issued"] = pd.to_datetime(df["enforcement_action_issued"], errors="coerce")
+df["enforcement_action_issued"] = df["enforcement_action_issued"].dt.strftime("%Y-%m-%d")
+
+# Save the updated CSV file
+df.to_csv("static/actions2.csv", index=False)
