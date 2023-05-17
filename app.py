0dab00ba58787932bee11ce72bc4daf15e7d3f07
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-05-17 22:54:16 +0000

    still working on inspection type name and count

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def detail(slug):
     inspections_count = len(Inspection.select().where(Inspection.slug==slug))
     actions_count = len(Action.select().where(Action.slug==slug))
     county = inspections[0].county
-    return render_template("detail.html", slug=slug, county=county, inspections=inspections, actions=actions, inspections_count=inspections_count, actions_count=actions_count, commonest_inspections = commonest_inspections, commonest_inspection_type=commonest_inspection_type)
+    return render_template("detail.html", slug=slug, county=county, inspections=inspections, actions=actions, inspections_count=inspections_count, actions_count=actions_count)
 
 # if i'm capturing a value in a url, it needs to be surrounded by <> in the route
 @app.route('/site/<site_no>')
